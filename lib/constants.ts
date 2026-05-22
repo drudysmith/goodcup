d54aeaee72c4100358222482771bb9022925e68c
drudysmith <d.rudy.smith@gmail.com>
2026-05-22 12:06:02 -0700

    added blog

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,7 @@
 export const navLinks = [
   { name: 'WHAT IS MANDUST', href: '/mandust' },
   { name: 'OUR DADDY-DAUGHTER SHOP', href: '/about' },
+  { name: 'BLOG', href: '/blog' },
 ];
 
 export const FEATURED_PRODUCT_IDENTIFIERS = {
