63f80a4e49c813f653929509542df855231e8b37
Rudy Smith <d.rudy.smith@gmail.com>
2026-08-06 12:14:20 -0700

    Add Daily Goodcup 60 QR redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/dailygoodcup60",
+        destination: "https://goodcup.me",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
